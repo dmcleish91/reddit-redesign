68f7f1bfb56f0d4890b823d1ebb9405494a196f6
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2023-11-05 02:45:46 -0500

    Fix border styling and import order in Feed component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,12 +1,13 @@
-import { posts } from '@/data/mock';
 import Post from '@/components/Post';
+import { posts } from '@/data/mock';
+
 import SortCreatePostMenu from './SortCreatePostMenu';
 
 function Feed() {
   return (
     <>
       <SortCreatePostMenu />
-      <div className='divide-y divide-slate-300 rounded outline outline-1 outline-slate-600'>
+      <div className='divide-y divide-slate-300 rounded border border-slate-300'>
         {posts.map(({ postAge, subredditName, title, username }) => (
           <Post key={title} title={title} subredditName={subredditName} username={username} postAge={postAge} />
         ))}
