cadbc9589c8a7d806fbccedece91ec533310c640
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2023-11-10 19:17:12 -0500

    Refactor Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,17 +4,15 @@ import { Outlet } from 'react-router-dom';
 
 function Home() {
   return (
-    <>
-      <div className='flex flex-grow flex-row bg-gray-100' id='main-content'>
-        <div className='w-full space-y-3 p-3 2xl:w-10/12'>
-          <Outlet />
-        </div>
-        <div className='hidden space-y-3 p-3 2xl:block 2xl:w-2/12'>
-          <HomeCard />
-          <TrendingCard />
-        </div>
+    <div className='flex flex-grow flex-row bg-gray-100' id='main-content'>
+      <div className='w-full space-y-3 p-3 2xl:w-10/12'>
+        <Outlet />
       </div>
-    </>
+      <div className='hidden space-y-3 p-3 2xl:block 2xl:w-2/12'>
+        <HomeCard />
+        <TrendingCard />
+      </div>
+    </div>
   );
 }
 export default Home;
